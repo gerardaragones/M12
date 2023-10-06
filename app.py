9ceb7e695897965edd59b7a3cfb0d8c3b523928d
Gerard <gearci@fp.insjoaquimmir.cat>
2023-10-06 19:32:55 +0200

    0.1.2-sqlite

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+
+import sqlite3
+from flask import g
+
+DATABASE = 'database.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+        db.row_factory = sqlite3.Row
+    return db
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template('app.html')
+
+@app.route('/products/list')
+def prod():
+    products = query_db("select * from products")
+    return render_template("list.html", products = products)
+
+
+if __name__ == 'main':
+    app.run(debug = True)
