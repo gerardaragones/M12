856f626161c1f1a712ada58e660bd0632fe2a1d8
Gerard <gearci@fp.insjoaquimmir.cat>
2023-10-20 21:18:49 +0200

    0.1.3-forms

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, url_for, render_template, request
+from datetime import datetime
+from werkzeug.utils import secure_filename
 
 import sqlite3
 from flask import g
+
+
+app = Flask(__name__)
 
 DATABASE = 'database.db'
 
@@ -18,17 +23,52 @@ def query_db(query, args=(), one=False):
     cur.close()
     return (rv[0] if rv else None) if one else rv
 
-app = Flask(__name__)
+def allowed_file(filename):
+    ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS 
 
 @app.route('/')
-def hello():
+def home():
     return render_template('app.html')
 
 @app.route('/products/list')
-def prod():
+def productslist():
     products = query_db("select * from products")
-    return render_template("list.html", products = products)
+    return render_template("/products/list.html", products = products)
+
+@app.route('/products/create', methods = ['POST', 'GET'])
+def producstcreate():
+    if request.method == 'GET':
+        return render_template("/products/create.html")
+    else:
+        titol = request.form['titol']
+        desc = request.form['description']
+        preu = request.form['preu']
+        cat_id = 1
+        sell_id = 1
+        created = datetime.now()
+        updated = datetime.now()
+        if 'foto' in request.files:
+            foto = request.files['foto']
+            if foto and allowed_file(foto.filename):
+                with get_db() as db:
+                    db.execute(
+                        "INSERT INTO products (title, description, photo, price, category_id, seller_id, created, updated) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+                        (titol, desc, foto.filename, preu, cat_id, sell_id, created, updated)
+                    )   
+        
+        return redirect(url_for('productslist'))
+
+@app.route('/products/read/<int:product_id>', methods = ['GET'])
+def readprod(product_id):
+    with get_db() as db:
+        res = db.execute("SELECT * FROM products WHERE id = ?", (product_id,))
+        items = res.fetchone()
+    
+    return render_template('/products/read.html', product = items)
+
 
 
 if __name__ == 'main':
-    app.run(debug = True)
+    app.run(debug = True)   
